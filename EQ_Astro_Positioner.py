d752119c912a69944e8cd360a15e9e36cf32c843
The Daxie Coder <isaac.d.ritchie@gmail.com>
2026-01-30 19:24:09 +0000

    Feat: Added current foreseen modules.

---
+++ EQ_Astro_Positioner.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+"""Imports"""
+import json # Data parsing for API.
+import requests # API module.
+import os
+import math # for EQ geometry
+import time
+import logging
