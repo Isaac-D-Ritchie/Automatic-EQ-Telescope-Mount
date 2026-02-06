5748040f03a48ca25389882b57e2906a33971fda
The Daxie Coder <isaac.d.ritchie@gmail.com>
2026-02-06 20:41:54 +0000

    Imported astropy, and added input protectiong modules for later - added time test for astopy

--- EQ_Astro_Positioner.py
+++ EQ_Astro_Positioner.py
@@ -8,10 +8,49 @@ ASTEPS is a person portfolio project where i will me combining my electrical eng
 This will be a designed as a personal astrophotography and observation tool for my 135 diameter, 800mm Telescope with a manual equatorial telescope
 """
 
-"""Imports"""
-import json # Data parsing for API.
-import requests # API module.
-import os
-import math # for EQ geometry
-import time
-import logging
+"""Imports & Modules"""
+from astropy.time import Time
+
+
+"""Functions"""
+#Get safe input
+def safe_input(prompt: str) -> str:
+    """
+    Function to validate safe input
+    Arguments:
+      prompt - string for input prompt
+    Return:
+        Input data or empty string if error occurs
+    """
+    try:
+        return input(prompt)
+    except (KeyboardInterrupt, EOFError):
+        print("\nInput Error")
+        return ""
+
+#Get non-empty string
+def get_non_empty_string(prompt: str) -> str:
+    """
+    Function to get non empty string using while loop
+    Utilizes safe_input function for input
+    Arguments:
+        prompt - string for input prompt
+    Return:
+        Non-empty string
+    """
+    while True:
+        raw_input: str = safe_input(prompt)
+        sanitized_input: str = raw_input.strip()
+
+        if sanitized_input == "":
+            print("\nError: Input cannot be empty")
+            continue
+        else:
+            return sanitized_input
+
+
+"""Main code"""
+if __name__ == "__main__":
+    print("Start Program")
+    t = Time.now()
+    print(t)
