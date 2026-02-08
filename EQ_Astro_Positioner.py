d2ac647b06268f8dff09d02dd7edb9ff9d3ee80e
The Daxie Coder <isaac.d.ritchie@gmail.com>
2026-02-08 05:28:38 +0000

    Added to project info docstring

--- EQ_Astro_Positioner.py
+++ EQ_Astro_Positioner.py
@@ -2,11 +2,12 @@
 Astronomical Equatorial Positioning System (ASTEPS)
 
 ASTEPS is a person portfolio project where i will me combining my electrical
-engineering knowledge with Python programming to create an automated telescope
-mounting system which can be retrofitted to manual equatorial telescope mounts.
+engineering knowledge with Python programming to create an automated mount
+positioning system which can be retrofitted to manual equatorial telescope
+mounts.
 
-This will be a designed as a personal astrophotography and observation tool for 
-my 135 diameter, 800mm Telescope with a manual equatorial mount.
+This will be a designed as a personal astrophotography and observation tool
+for my 135 diameter, 800mm Telescope with a manual equatorial mount.
 """
 
 #=====================================================================#
