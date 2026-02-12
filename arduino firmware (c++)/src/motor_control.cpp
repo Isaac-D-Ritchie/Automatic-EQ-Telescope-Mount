06f84b951fb79eef9e8b3c6acb59fb1d07c7fd91
The Daxie Coder <isaac.d.ritchie@gmail.com>
2026-02-12 22:54:38 +0000

    Added code to move motor clockwise for testing

--- arduino firmware (c++)/src/motor_control.cpp
+++ arduino firmware (c++)/src/motor_control.cpp
@@ -2,26 +2,54 @@
 
 //Defining pins
 
+//Motor engage pin
+#define en_pin 4
+
 //Motor 1
 #define step_pin_1 2
 #define dir_pin_1 3
-#define en_pin_1 4
 
 //Motor 2
 #define step_pin_2 5
 #define dir_pin_2 6
-#define en_pin_2 7
 
 //LEDs
-#define red_led 8
+#define red_led 7
 #define green_led 9
+
+
+//Functions
+void move_motor_1_clockwise(int steps, int delay)
+{
+    for (int i = 0; i < steps; i++)
+    {
+        digitalWrite(en_pin, LOW);
+        digitalWrite(red_led, LOW);
+        digitalWrite(dir_pin_1, HIGH)
+        digitalWrite(step_pin_1, HIGH);
+        delayMicroseconds(delay);
+        digitalWrite(step_pin_1, LOW);
+        delayMicroseconds(delay);
+        digitalWrite(red_led, HIGH);
+    }
+}
 
 // Setup code (runs once)
 void setup() {
-  
+  digitalWrite(en_pin, HIGH); //HIGH = no power
+  pinMode(step_pin_1, OUTPUT);
+  pinMode(dir_pin_1, OUTPUT);
+  pinMode(en_pin, OUTPUT);
+  pinMode(step_pin_2, OUTPUT);
+  pinMode(dir_pin_2, OUTPUT);
+  pinMode(red_led, OUTPUT);
+  pinMode(green_led, OUTPUT);
 }
+
+
 
 // Main script
 void loop() {
-  
+  move_motor_1_clockwise(3200, 800);
+  delay(1000);
 }
