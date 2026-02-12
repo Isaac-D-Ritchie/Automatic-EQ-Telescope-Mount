4c633b8f8ced8f8d353da3942f2e7923eedbdc7f
The Daxie Coder <isaac.d.ritchie@gmail.com>
2026-02-12 22:30:36 +0000

    Added pin definitions

--- arduino firmware (c++)/src/motor_control.cpp
+++ arduino firmware (c++)/src/motor_control.cpp
@@ -1,8 +1,20 @@
 #include <Arduino.h>
 
-// Functions
-int myFunction(int, int);
+//Defining pins
 
+//Motor 1
+#define step_pin_1 2
+#define dir_pin_1 3
+#define en_pin_1 4
+
+//Motor 2
+#define step_pin_2 5
+#define dir_pin_2 6
+#define en_pin_2 7
+
+//LEDs
+#define red_led 8
+#define green_led 9
 
 // Setup code (runs once)
 void setup() {
