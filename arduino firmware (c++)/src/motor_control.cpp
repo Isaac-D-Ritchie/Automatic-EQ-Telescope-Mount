cb376e27025f8b0d4426c678fbfdf0e7fb34f0b9
The Daxie Coder <isaac.d.ritchie@gmail.com>
2026-06-23 15:29:18 +0100

    Feat: Added AccelStepper functionality for better torque control

--- arduino firmware (c++)/src/motor_control.cpp
+++ arduino firmware (c++)/src/motor_control.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <AccelStepper.h>
 
 //Defining pins
 
@@ -18,8 +19,11 @@
 #define stick_x A1
 #define stick_y A2
 
+AccelStepper motor1(AccelStepper::DRIVER, step_pin_1, dir_pin_1);
+AccelStepper motor2(AccelStepper::DRIVER, step_pin_2, dir_pin_2);
 
 //Functions
+//Custom motor control (NON AccelStepper)
 void move_motor(int steps, int pulse_delay, int step_pin, int dir_pin, bool dir) {
     digitalWrite(dir_pin, dir); //HIGH = clockwise LOW = anti-clockwise
 
@@ -53,8 +57,14 @@ void setup() {
   pinMode(en_pin_2, OUTPUT);
   pinMode(blue_led, OUTPUT);
   pinMode(red_led, OUTPUT);
-  digitalWrite(en_pin_1, HIGH); //Motor 1 power (off)
-  digitalWrite(en_pin_2, HIGH); //Motor 2 power (off)
+
+  motor1.setMaxSpeed(3000);
+  motor1.setAcceleration(1000);
+  enable_motor(en_pin_1);
+
+  motor2.setMaxSpeed(3000);
+  motor2.setAcceleration(1000);
+  enable_motor(en_pin_2);
 
   Serial.begin(9600);
 }
@@ -79,35 +89,26 @@ void loop() {
 
   //Motor 1
   if (x > 600) {
-    enable_motor(en_pin_1);
-    move_motor(10, 650, step_pin_1, dir_pin_1, HIGH); //Clockwise
+    motor1.setSpeed(1000);
+  }
+  else if (x < 400) {
+    motor1.setSpeed(-1000);
   }
   else {
-    disable_motor(en_pin_1);
-  }
-
-  if (x < 400) {
-    enable_motor(en_pin_1);
-    move_motor(10, 650, step_pin_1, dir_pin_1, LOW); //Anti-clockwise
-  }
-  else {
-    disable_motor(en_pin_1);
+    motor1.setSpeed(0);
   }
 
   //Motor 2
   if (y > 600) {
-    enable_motor(en_pin_2);
-    move_motor(10, 650, step_pin_2, dir_pin_2, HIGH); //Clockwise
+    motor2.setSpeed(1000);
+  }
+  else  if (y < 400) {
+    motor2.setSpeed(-1000);
   }
   else {
-    disable_motor(en_pin_2);
+    motor2.setSpeed(0);
   }
   
-  if (y < 400) {
-    enable_motor(en_pin_2);
-    move_motor(10, 650, step_pin_2, dir_pin_2, LOW); //Anti-clockwise
-  }
-  else {
-    disable_motor(en_pin_2);
-  }
+  motor1.run();
+  motor2.run();
 }
