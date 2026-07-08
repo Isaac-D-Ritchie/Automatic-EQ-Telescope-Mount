849eab9084f3d8ab43161c5e85a5b135bc81e157
The Daxie Coder <isaac.d.ritchie@gmail.com>
2026-07-08 22:23:08 +0100

    Feat: Added menu scroll functionality and fixed bitrate expectation for serial monitoring

--- arduino firmware (c++)/src/motor_control.cpp
+++ arduino firmware (c++)/src/motor_control.cpp
@@ -31,7 +31,7 @@ AccelStepper motor1(AccelStepper::DRIVER, step_pin_1, dir_pin_1);
 AccelStepper motor2(AccelStepper::DRIVER, step_pin_2, dir_pin_2);
 
 
-//Enums
+//Enums and variables
 //Display state options
 enum screen_state {
   Logo,
@@ -57,6 +57,11 @@ enum menu_items {
   select_settings
 };
 menu_items selected_menu_item = select_manual;
+//Joystick variables - global
+int joystick_x = 0;
+int joystick_y = 0;
+bool joystick_btn = HIGH;
+bool joystick_stop = false; //Stops endless scrolling in menu
 
 
 //Functions
@@ -68,6 +73,60 @@ void enable_motor(int en_pin) {
 void disable_motor(int en_pin) {
   digitalWrite(en_pin, HIGH); //Cuts TMC 2209 power
   digitalWrite(blue_led, HIGH); //Cuts motor LED
+}
+//Function to update joystick in loop
+void update_joystick() {
+  joystick_btn = digitalRead(stick_sw);
+  joystick_x = analogRead(stick_x);
+  joystick_x -= 524; //Resting value adjustment x
+  if (abs(joystick_x) <25) {
+    joystick_x = 0;
+  }
+  joystick_y = analogRead(stick_y);
+  joystick_y -= 509; //Resting value adjustment y
+  if (abs(joystick_y) <25) {
+    joystick_y = 0;
+  }
+}
+//Joystick Option Selection function 
+void option_selection() {
+  if (!joystick_stop) {
+    if (joystick_y < -300) { //Move up
+      joystick_stop = true;
+      switch (selected_menu_item)
+      {
+      case select_settings:
+        selected_menu_item = select_track;
+        break;
+    
+      case select_track:
+        selected_menu_item = select_manual;
+        break;
+    
+      case select_manual:
+        break;
+      }
+    }
+    else if (joystick_y > 300) { //Move down
+      joystick_stop = true;
+      switch (selected_menu_item)
+      {
+      case select_manual:
+        selected_menu_item = select_track;
+        break;
+    
+      case select_track:
+        selected_menu_item = select_settings;
+        break;
+
+      case select_settings:
+        break;
+      }
+    }
+  }
+  if (abs(joystick_y) < 100) {
+    joystick_stop = false;
+  }
 }
 //Display Screen function
 void draw_logo() { //Logo Screen
@@ -94,7 +153,7 @@ void draw_menu() { //Menu control screen
   do
   {
     display.setFont(u8g2_font_6x10_tr);
-    display.drawStr(10, 10, "Menu");
+    display.drawStr(50, 10, "Menu");
 
     if (selected_menu_item == select_manual)
       display.drawStr(0,25,"> Manual <");
@@ -152,25 +211,15 @@ void setup() {
 // Main script
 void loop() {
   digitalWrite(red_led, LOW); //Power indicator
-  //Joystick controls
-  bool button = digitalRead(stick_sw);
-  int x = analogRead(stick_x);
-  x -= 524; //Resting value adjustment x
-  if (abs(x) <25) {
-    x = 0;
-  }
-  int y = analogRead(stick_y);
-  y -= 509; //Resting value adjustment y
-  if (abs(y) <25) {
-    y = 0;
-  }
+  update_joystick(); //Updates joystick position
+
   //Prints joystick reading to serial output
   Serial.print("X: ");
-  Serial.print(x);
+  Serial.print(joystick_x);
   Serial.print("  Y: ");
-  Serial.print(y);
+  Serial.print(joystick_y);
   Serial.print("  Button: ");
-  Serial.println(button);
+  Serial.println(joystick_btn);
 
 
   //Switch cases for display
@@ -181,6 +230,7 @@ void loop() {
       break;
 
     case Menu:
+      option_selection();
       draw_menu();
       break;
 
@@ -201,8 +251,8 @@ void loop() {
   }
 
   //Variable motor control from joystick
-  float variable_x = constrain(x / 500.0, -1.0, 1.0); //Convert value to between 1 and -1
-  float variable_y = constrain(y / 500.0, -1.0, 1.0);
+  float variable_x = constrain(joystick_x / 500.0, -1.0, 1.0); //Convert value to between 1 and -1
+  float variable_y = constrain(joystick_y / 500.0, -1.0, 1.0);
   float control_expo = 2.2; //Joystick response curve and calculation
   float control_x = (variable_x >= 0 ? 1 : -1) * pow(abs(variable_x), control_expo);
   float control_y = (variable_y >= 0 ? 1 : -1) * pow(abs(variable_y), control_expo);
