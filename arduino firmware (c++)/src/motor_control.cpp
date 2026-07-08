5087692f34d25abbbd853a907482477f1997897d
The Daxie Coder <isaac.d.ritchie@gmail.com>
2026-07-08 16:03:34 +0100

    Feat: added menu screen and switched to page buffer system to save arduino SRAM after memory space issues

--- arduino firmware (c++)/src/motor_control.cpp
+++ arduino firmware (c++)/src/motor_control.cpp
@@ -24,8 +24,8 @@
 
 
 //Hardware Objects
-//128 x 64 OLED Screen (Defined as 'display')
-U8G2_SH1106_128X64_NONAME_F_HW_I2C display(U8G2_R0, U8X8_PIN_NONE); 
+//128 x 64 OLED Screen (Defined as 'display') - Using refresh page system to save SRAM 
+U8G2_SH1106_128X64_NONAME_1_HW_I2C display(U8G2_R0, U8X8_PIN_NONE);
 //Assigning AccelStepper pins for motor drivers
 AccelStepper motor1(AccelStepper::DRIVER, step_pin_1, dir_pin_1);
 AccelStepper motor2(AccelStepper::DRIVER, step_pin_2, dir_pin_2);
@@ -52,11 +52,11 @@ enum telescope_status {
 telescope_status current_telescope_status =  Idle;
 //Menu Options
 enum menu_items {
-  menu_manual,
-  menu_track,
-  menu_settings
+  select_manual,
+  select_track,
+  select_settings
 };
-menu_items selected_item = menu_manual;
+menu_items selected_menu_item = select_manual;
 
 
 //Functions
@@ -71,24 +71,46 @@ void disable_motor(int en_pin) {
 }
 //Display Screen function
 void draw_logo() { //Logo Screen
-  display.clearBuffer();
-  display.setFont(u8g2_font_ncenB18_tr);
-  display.drawStr(10, 30, "ASTEPS");
-  display.setFont(u8g2_font_6x10_tr);
-  display.drawStr(20, 50, "Press Joystick");
-  display.drawStr(28, 60, "to continue");
-  display.sendBuffer();
-
-  if (digitalRead(stick_sw) == LOW) { //Joystick switch detection
-    current_display_status = Menu;
+  {
+    display.firstPage();
+    do
+    {
+      display.setFont(u8g2_font_ncenB18_tr);
+      display.drawStr(10, 30, "ASTEPS");
+      display.setFont(u8g2_font_6x10_tr);
+      display.drawStr(20, 50, "Press Joystick");
+      display.drawStr(28, 60, "to continue");
+    }
+    while (display.nextPage());
+    {
+    if (digitalRead(stick_sw) == LOW) { //Joystick switch detection
+    current_display_status = Menu; }
+    }
   }
 }
 void draw_menu() { //Menu control screen
-  display.clearBuffer();
-  display.setFont(u8g2_font_ncenB18_tr);
-  display.drawStr(10, 30, "Menu");
-  display.sendBuffer();
+
+  display.firstPage();
+  do
+  {
+    display.setFont(u8g2_font_6x10_tr);
+    display.drawStr(10, 10, "Menu");
+
+    if (selected_menu_item == select_manual)
+      display.drawStr(0,25,"> Manual <");
+    else display.drawStr(0,25,"  Manual");
+
+    if (selected_menu_item == select_track)
+      display.drawStr(0,40,"> Tracking <");
+    else display.drawStr(0,40,"  Tracking");
+
+    if (selected_menu_item == select_settings) 
+      display.drawStr(0,55,"> Settings <");
+    else display.drawStr(0,55,"  Settings");
+
+  } while (display.nextPage());
 }
+
 void draw_manual() { //Manual control screen
  //Plans to make joystick tracking and current position <-----------------------------
 }
